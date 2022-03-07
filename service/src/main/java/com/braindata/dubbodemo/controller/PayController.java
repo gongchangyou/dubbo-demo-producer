1ec5211cff1e0ab9335e1a05e2a66d673a2393e5
gongchangyou <gongchangyou@58.com>
2022-03-07 10:39:17 +0800

    exec

--- service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
+++ service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
@@ -3,13 +3,19 @@ package com.braindata.dubbodemo.controller;
 import com.braindata.dubbodemo.intf.StuRpcService;
 import com.braindata.rocketmq.Producer;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Slf4j
 @RestController
@@ -36,5 +42,42 @@ public class PayController {
             @RequestParam("msg") String msg
     ){
         return producer.sendMsg(msg)? "true":"false";
+    }
+
+    /**
+     * 执行shell命令
+     */
+    @RequestMapping(value = "/exec", method = RequestMethod.POST)
+    public String exec(
+            @RequestParam(value = "command") String command,
+            @RequestParam(value = "path", required = false) String path
+    ) {
+        log.info("command=" + command);
+        execCommand("sh " + path);
+        execCommand(command);
+
+        return "{}";
+    }
+
+    private int execCommand(String command) {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(command);
+            val result = process.waitFor();
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                log.info("input={}", line);
+            }
+            input.close();
+
+            log.info("result={}", result);
+
+            return result;
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
     }
 }
