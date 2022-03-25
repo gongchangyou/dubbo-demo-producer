b46a35d1e2157554c96f2965a4086159b38bdaac
gongchangyou <gongchangyou@58.com>
2022-03-25 16:27:29 +0800

    mybatis plus

--- service/src/main/java/com/braindata/dubbodemo/DubboDemoApplication.java
+++ service/src/main/java/com/braindata/dubbodemo/DubboDemoApplication.java
@@ -1,9 +1,8 @@
 package com.braindata.dubbodemo;
 
-import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import com.braindata.rocketmq.MessageSource;
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -13,6 +12,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 //@EnableNacosDiscovery
 //@DubboComponentScan(value = "com.braindata.dubbodemo.impl")
 @EnableDubbo(scanBasePackages = "com.braindata.dubbodemo.impl")
+@MapperScan("com.braindata.dubbodemo.repository.db.mapper")
 public class DubboDemoApplication {
 
     public static void main(String[] args) {
